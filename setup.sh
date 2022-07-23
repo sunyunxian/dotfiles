fde43b38b0581538d9f0b7ece2d89739773af881
sunyunxian <personal_sunyunxian@foxmail.com>
2022-07-23 10:52:20 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ DOTFILES_ARRAY=(
     .zshrc
     .bahrc
     .p10k.zsh
-    .config
+    # .config
     .gitconfig
     .gitignore
     .aliasrc
@@ -20,9 +20,9 @@ for i in "${DOTFILES_ARRAY[@]}"; do
     if [[ -a "${HOME}/${i}" ]];then
         echo "rm ${HOME}/${i}"
         rm -rf "${HOME}/${i}"
-        ln -s "${CURRENT_PATH}/${i}" "${HOME}/${i}"
+        cat "${CURRENT_PATH}/${i}" > "${HOME}/${i}"
     else
-        ln -s "${CURRENT_PATH}/${i}" "${HOME}/${i}"
+        cat "${CURRENT_PATH}/${i}" > "${HOME}/${i}"
     fi
 done
 
